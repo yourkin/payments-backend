66df6436889895b33c2c500810d8896a9b721c17
Andrew Yourkin <andrew.yourkin@gmail.com>
2019-04-29 19:17:52 +0300

    Initial commit

---
+++ paymentsbackend/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from rest_framework_jwt.views import obtain_jwt_token
+
+
+urlpatterns = [
+    path('token-auth/', obtain_jwt_token),
+    path('admin/', admin.site.urls),
+]
